204a2e03b764ee370d8ba66540f44ded6702b710
silent06 <44829491+silent06@users.noreply.github.com>
2023-04-05 20:11:13 -0500

    Add files via upload

---
+++ xmls/quicklaunch.php
@@ -0,0 +1,35 @@
+<?php
+
+$launchId = $_GET['cpu'];
+
+
+$connect = mysqli_connect("root.silent.hosted.nfoservers.com", "silentwebhost", "", "silentwebhost_DizzyV6");
+
+if($connect) {
+
+	if (isset($launchId)) {
+
+		$sql = "SELECT * FROM `users` WHERE `cpu`='" . $launchId . "' LIMIT 1";
+		$result = $connect->query($sql);
+				
+		if ($result->num_rows > 0) 
+		{
+			while($row = $result->fetch_assoc()) {
+				
+				$titleId = $row["qtitleid"];
+				echo $titleId;
+				$connect->query("UPDATE `users` SET `qtitleid`='12345678' WHERE `cpu`='" . $launchId . "' LIMIT 1"); //update Launch ID
+			}
+		}
+		else
+		{
+			echo "Failed";
+		}
+
+	}
+
+}else{
+
+	echo "Failed";
+}
+?>
